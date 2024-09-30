4756a58ef7927adb5de758580d308df1647c59ca
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2024-09-30 16:27:00 -0300

    .

---
+++ example-app/database/seeders/VoucherCoordinatesSeeder.php
@@ -0,0 +1,289 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\VoucherCoordinate;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class VoucherCoordinatesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        VoucherCoordinate::insert([
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '1',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '2',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '3',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '4',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '5',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '6',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '7',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '8',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '9',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '10',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '11',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '12',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '13',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '14',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '15',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '16',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '17',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '18',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '19',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '20',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '21',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '22',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '23',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '24',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '25',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '26',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '27',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '28',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '29',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '30',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '31',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '32',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '33',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '34',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '35',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '36',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '37',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '38',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '39',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '40',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '41',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '42',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '43',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '44',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '45',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '46',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '47',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '48',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '49',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '50',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '51',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '52',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '53',
+            ],
+            [
+                'latitudine_1' => '-23.522656785035263',
+                'longitudine_1' => '-46.709297929225315',
+                'voucher_id' => '54',
+            ],
+        ]);
+    }
+}
---
+++ example-app/database/seeders/VoucherCuponsSeeder.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Voucher;
+
+class VoucherCuponsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Voucher::insert([
+            [
+                'voucher' => '001ABC'
+            ],
+            [
+                'voucher' => '002ABC'
+            ],
+            [
+                'voucher' => '003ABC'
+            ],
+            [
+                'voucher' => '004ABC'
+            ],
+            [
+                'voucher' => '005ABC'
+            ],
+            [
+                'voucher' => '006ABC'
+            ],
+            [
+                'voucher' => '007ABC'
+            ],
+            [
+                'voucher' => '008ABC'
+            ],
+            [
+                'voucher' => '009ABC'
+            ],
+            [
+                'voucher' => '010ABC'
+            ],
+            [
+                'voucher' => '011ABC'
+            ],
+            [
+                'voucher' => '012ABC'
+            ],
+            [
+                'voucher' => '013ABC'
+            ],
+            [
+                'voucher' => '014ABC'
+            ],
+            [
+                'voucher' => '015ABC'
+            ],
+            [
+                'voucher' => '016ABC'
+            ],
+            [
+                'voucher' => '017ABC'
+            ],
+            [
+                'voucher' => '018ABC'
+            ],
+            [
+                'voucher' => '019ABC'
+            ],
+            [
+                'voucher' => '020ABC'
+            ],
+            [
+                'voucher' => '021ABC'
+            ],
+            [
+                'voucher' => '022ABC'
+            ],
+            [
+                'voucher' => '023ABC'
+            ],
+            [
+                'voucher' => '024ABC'
+            ],
+            [
+                'voucher' => '025ABC'
+            ],
+            [
+                'voucher' => '026ABC'
+            ],
+            [
+                'voucher' => '027ABC'
+            ],
+            [
+                'voucher' => '028ABC'
+            ],
+            [
+                'voucher' => '029ABC'
+            ],
+            [
+                'voucher' => '030ABC'
+            ],
+            [
+                'voucher' => '031ABC'
+            ],
+            [
+                'voucher' => '032ABC'
+            ],
+            [
+                'voucher' => '033ABC'
+            ],
+            [
+                'voucher' => '034ABC'
+            ],
+            [
+                'voucher' => '035ABC'
+            ],
+            [
+                'voucher' => '036ABC'
+            ],
+            [
+                'voucher' => '037ABC'
+            ],
+            [
+                'voucher' => '038ABC'
+            ],
+            [
+                'voucher' => '039ABC'
+            ],
+            [
+                'voucher' => '040ABC'
+            ],
+            [
+                'voucher' => '041ABC'
+            ],
+            [
+                'voucher' => '042ABC'
+            ],
+            [
+                'voucher' => '043ABC'
+            ],
+            [
+                'voucher' => '044ABC'
+            ],
+            [
+                'voucher' => '045ABC'
+            ],
+            [
+                'voucher' => '046ABC'
+            ],
+            [
+                'voucher' => '047ABC'
+            ],
+            [
+                'voucher' => '048ABC'
+            ],
+            [
+                'voucher' => '049ABC'
+            ],
+            [
+                'voucher' => '050ABC'
+            ],
+            [
+                'voucher' => '051ABC'
+            ],
+            [
+                'voucher' => '052ABC'
+            ],
+            [
+                'voucher' => '053ABC'
+            ],
+            [
+                'voucher' => '054ABC'
+            ],
+        ]);
+    }
+}
